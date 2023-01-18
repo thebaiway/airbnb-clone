ecf6dcbb2b3c698ce69ace8f266b79c05c1ecb39
Bailee <bailee.roby@gmail.com>
2023-01-17 22:34:02 -0800

    updating app.js using props

--- src/App.js
+++ src/App.js
@@ -2,13 +2,51 @@ import './App.css';
 import Navbar from './Components/Navbar.js'
 import Hero from './Components/Hero.js'
 import Card from './Components/Card.js'
+import Katie from './images/katie-zaferes.png'
 
 function App() {
   return (
     <div className="App">
         <Navbar />
         <Hero />
-        <Card />
+      <div className="app--card_div">
+        <Card 
+            img = {Katie}
+            status = 'SOLD OUT'
+            rating = "5.0"
+            reviewCount = {6}
+            country = "USA"
+            title = "Life lessons with Katie Zaferes"
+            price = "136"
+        />
+          <Card 
+            img = {Katie}
+            status = 'SOLD OUT'
+            rating = "5.0"
+            reviewCount = {6}
+            country = "USA"
+            title = "Life lessons with Katie Zaferes"
+            price = "136"
+        />
+        <Card 
+            img = {Katie}
+            status = 'SOLD OUT'
+            rating = "5.0"
+            reviewCount = {6}
+            country = "USA"
+            title = "Life lessons with Katie Zaferes"
+            price = "136"
+        />
+        <Card 
+            img = {Katie}
+            status = 'SOLD OUT'
+            rating = "5.0"
+            reviewCount = {6}
+            country = "USA"
+            title = "Life lessons with Katie Zaferes"
+            price = "136"
+        />
+      </div>
     </div>
   );
 }
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,19 +1,18 @@
-import cardImg from '../images/katie-zaferes.png'
 import cardStar from '../images/star.png'
 
-export default function Card () {
+export default function Card (props) {
     return (
     <div className="card--div">
-            <img className="card--div_img" src={cardImg} alt=""></img>
-            <p className="card--div_sold-out">SOLD OUT</p>
-            <div className="card--div_star">
-                <img className="card--div_star-img" src={cardStar} alt=""></img>
-                <p className="card--div_star-desc">5.0<scan> (6) • USA </scan></p>
-            </div>
-            <div className="card--div_price">
-            <p>Life lessons with Katie Zaferes</p>
-            <p><b>From $136</b> / person</p>
-            </div>
+             <img className="card--div_img" src={props.img} alt=""></img>
+             <p className="card--div_sold-out">{props.status}</p>
+        <div className="card--div_star">
+            <img className="card--div_star-img" src={cardStar} alt=""></img>
+            <p className="card--div_star-desc">{props.rating}<scan> ({props.reviewCount}) • {props.country} </scan></p>
+        </div>
+        <div className="card--div_price">
+            <p>{props.title}</p>
+            <p><b>From ${props.price}</b> / person</p>
+        </div>
     </div>
     )
 }
